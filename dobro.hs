a5c7199dcba49008817410e4face24a9b7d65f74
erikdiniz <erikdinizbeserra@gmail.com>
2024-03-14 21:57:42 -0300

    update

--- dobro.hs
+++ dobro.hs
@@ -1,4 +1,7 @@
 -- Função que recebe um inteiro e retorna o dobro desse inteiro
-
 dobro:: Int -> Int
 dobro n = 2*n
+
+-- Função que calcula o quadruplo de um inteiro usando a função dobro
+quad:: Int -> Int
+quad n = 2*(dobro n)
--- par.hs
+++ par.hs
@@ -1,4 +1,3 @@
 -- Função que recebe um inteiro e retorna true caso o inteiro seja par
-
 ehPar:: Int -> Bool
 ehPar n = if mod n 2 == 0 then True else False
