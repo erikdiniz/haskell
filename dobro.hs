3c45a7b27be09c359641bf08c77b1aea06aedfd6
erikdiniz <erikdinizbeserra@gmail.com>
2024-04-03 21:39:51 -0300

    update dobro

--- dobro.hs
+++ dobro.hs
@@ -1,7 +1,17 @@
 -- Função que recebe um inteiro e retorna o dobro desse inteiro
-dobro:: Int -> Int
+dobro:: Double -> Double
 dobro n = 2*n
 
 -- Função que calcula o quadruplo de um inteiro usando a função dobro
-quad:: Int -> Int
+quad:: Double -> Double
 quad n = 2*(dobro n)
+
+-- Função que recebe dois números, dobra ambos e retorna a soma dos dobros
+doubleUs :: Double -> Double -> Double
+doubleUs x y = dobro x + dobro y
+
+--  Função que retorna o dobro de um número se ele for menor ou igual a 100.
+doubleSmallNumber :: Double -> Double
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
